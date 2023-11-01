2558f27f58c4f7a778d4c1cb17fa8840205e16ce
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-11-01 05:12:28 -0400

    -fix

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -4,4 +4,7 @@ const router = express.Router()
 
 router.post('/login', handleUserLogin)
 router.post('/register', handleUserRegistration)
+router.post('/check', (req, res) => {
+    res.send('hello')
+})
 module.exports = router
