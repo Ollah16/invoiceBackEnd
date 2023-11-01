4a81d1d08ccee7a6702c216f845c5382e00596de
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-11-01 04:38:58 -0400

    verC

--- model/invoiceModel.js
+++ model/invoiceModel.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
