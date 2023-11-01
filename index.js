4760d43449f10cb21620810527647492f1230e05
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-11-01 04:56:19 -0400

    va

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ const authenticateRoute = require('./Routes/userRoute')
 const invoiceRoute = require('./Routes/invoiceRoute')
 app.use('/authenticate', authenticateRoute)
 app.use('/invoice', invoiceRoute)
+app.post('/check', (req, res) => {
+    console.log('hi')
+})
 app.listen(process.env.PORT, () => {
 })
