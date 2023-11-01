ca943d0bf6c5eb710da67ec15db09af4a36e1bf2
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-11-01 05:21:12 -0400

    crash

--- index.js
+++ index.js
@@ -8,8 +8,5 @@ const authenticateRoute = require('./Routes/userRoute')
 const invoiceRoute = require('./Routes/invoiceRoute')
 app.use('/authenticate', authenticateRoute)
 app.use('/invoice', invoiceRoute)
-app.post('/check', (req, res) => {
-    res.send('helloCheck')
-})
 app.listen(process.env.PORT, () => {
 })
