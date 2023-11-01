87c13cb111844c5f76dffdb003b6fb9e8119b44c
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-11-01 02:52:32 -0400

    vercel1

--- model/invoiceModel.js
+++ model/invoiceModel.js
@@ -1,4 +1,10 @@
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
+});
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
